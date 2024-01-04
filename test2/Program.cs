43f1251f22089a1191f42e17cf52bcf5223901d3
Elen S <alexia.18@mail.ru>
2024-01-04 20:05:10 +0300

    Решила вторую задачу

---
+++ test2/Program.cs
@@ -0,0 +1,24 @@
+﻿int x = 3;
+int y = -5;
+if (x > 0)
+{
+    if (y > 0)
+    {
+        Console.WriteLine("1");
+    }
+    else
+    {
+        Console.WriteLine("4");
+    }
+}
+if (x < 0)
+{
+    if (y > 0)
+    {
+        Console.WriteLine("2");
+    }
+    else
+    {
+        Console.WriteLine("3");
+    }
+}
