a22c703c1bcbd16027a659aa1bc9fd3d10545b34
Elen S <alexia.18@mail.ru>
2024-01-04 20:11:57 +0300

    Решила третью задачу

---
+++ test3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 75;
+int firstDigit = n / 10;
+int secondDigit = n % 10;
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
