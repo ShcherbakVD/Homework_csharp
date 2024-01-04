9ef72a4a3872b3e015a236cea8a6ff08c85aa083
Elen S <alexia.18@mail.ru>
2024-01-04 19:51:48 +0300

    Решила первую задачу

---
+++ test1/Program.cs
@@ -0,0 +1,14 @@
+﻿int digit = 161;
+int res1 = digit % 7;
+int res2 = digit % 23;
+if (res1 == 0)
+{
+    if (res2 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    Console.WriteLine("Нет");
+}
