1087525c1c26291407d707ff17f45f65923b2287
Elen S <alexia.18@mail.ru>
2024-01-05 11:03:38 +0300

    Решила четвёртую задачу

---
+++ test4/Program.cs
@@ -0,0 +1,15 @@
+﻿long n = 1;
+if (n < 10)
+{
+    Console.Write(n);
+}
+else
+{
+    while (n >= 1)
+    {
+        long i = n % 10;
+        Console.Write(i);
+        Console.Write(',');
+        n = n / 10;
+    }
+}
